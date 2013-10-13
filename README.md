de55c88fdd6fa31242f0acedd9d4c69cc0dc192d
Cesar <cesarwbr@gmail.com>
2013-10-13 14:43:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JaggeryJS module boilerplate
+
+![](http://jaggeryjs.org/images/jaglogo.png)
+
+> An boilerplate for build your own module.
---
+++ scripts/my-module.js
@@ -0,0 +1,7 @@
+// Here you will define your module class with some logic
+var MyModule = function (arg1) {
+	this.arg1 = arg1;
+	this.getArg1 = function(){
+		return this.arg1;
+	};
+};
